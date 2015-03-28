ec9ef654b054c8af169e711f4348fcb84987d1a6
Jason Semko <jasonsemko@gmail.com>
2015-03-28 13:52:49 -0700

    first draft

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+
+grunt.initConfig({
+        sass: {
+            dist: {
+                options: {
+                    style: 'expand'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/scss',
+                    src: ['*.scss'],
+                    dest: 'dest/css',
+                    ext: '.css'
+                }]
+            }
+        },
+        htmlmin: {
+	        dist: { 
+	            options: { 
+	                removeComments: true,
+	                collapseWhitespace: true
+	            },
+	            files: { 
+	                'dest/index.html': 'src/index.html',
+	                'dest/coffee.html': 'src/coffee.html',
+	                'dest/pastries.html': 'src/pastries.html',
+	                'dest/location.html': 'src/location.html'
+	            }
+	        }
+        },
+        watch: {
+        	files: ['src/**/*'],
+        	tasks: ['default']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch'); grunt.loadNpmTasks('grunt-contrib-sass'); grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+    grunt.registerTask('default', ['sass','htmlmin']);
+
+
+};
